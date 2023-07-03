d081d81acdee2e89fe7d16a147e4ab9c8f5ca335
Mae Young <connormwyoung@gmail.com>
2023-07-03 02:41:43 +0200

    Changes to be committed: 	modified:   public/loginScript.js

--- public/loginScript.js
+++ public/loginScript.js
@@ -10,20 +10,12 @@ function login() {
     },
     body: JSON.stringify(creds)
   }).then((res) => res.json().then((response) => {
-  if(response.status == "Success") {
-    sessionStorage.setItem("sessionID", response.sessionID)
-    document.location = "/"
-  }else{
-    console.log(creds)
-    console.log(response)
-  }
-}))
-  //})
+    if(response.status == "Success") {
+      sessionStorage.setItem("sessionID", response.sessionID)
+      document.location = "/"
+    }else{
+      console.log(response)
+    }
+  }))
 }
 
-function sha512(str) {
-  /*return crypto.subtle.digest("SHA-512", new TextEncoder("utf-8").encode(str)).then(buf => {
-    return Array.prototype.map.call(new Uint8Array(buf), x=>(('00'+x.toString(16)).slice(-2))).join('');
-  });*/
-  return str
-}
